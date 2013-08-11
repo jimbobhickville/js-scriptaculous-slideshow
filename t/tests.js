6b63c435166717909c5467dd65a6f04a316c2e25
Greg Hill <jimbobhickville@gmail.com>
2013-08-11 16:55:33 -0700

    add some basic behavior tests
    Covers manual advancement and retreat and repeating behavior.
    Currently exposes a logic bug when going past the end of the
    list and back and then stopping repeat.  Will fix that next.

--- t/tests.js
+++ t/tests.js
@@ -1,8 +1,58 @@
-test("Local Slideshow", function () {
-  var slideShow = new Slideshow.Local('qunit-fixture', [
-    '<div>Slide 1</div>',
-    '<div>Slide 2</div>',
-    '<div>Slide 3</div>',
-    '<div>Slide 4</div>'
-  ], { delay: 0.05, repeat: false, randomize: false });
+test("Local Slideshow", 34, function () {
+  var visibleFrameCheck = function (total, visible) {
+    for (var i=1; i<=total; i++) {
+      if (i == visible) {
+        equal($('frame'+i).visible(), true, "Frame " + i + " is visible");
+      }
+      else {
+        equal($('frame'+i).visible(), false, "Frame " + i + " is hidden");
+      }
+    }
+  };
+
+  var frames = [
+    '<div id="frame1">Slide 1</div>',
+    '<div id="frame2">Slide 2</div>',
+    '<div id="frame3">Slide 3</div>',
+    '<div id="frame4">Slide 4</div>'
+  ];
+  var totalFrames = frames.length;
+
+  var slideShow = new Slideshow.Local('qunit-fixture', frames, {
+    hideEffect: Element.hide,
+    showEffect: Element.show,
+    delay: 30,
+    repeat: false,
+    randomize: false
+  });
+  slideShow.pause();
+
+  visibleFrameCheck(1, 1);
+
+  for (var i=2; i<=totalFrames; i++) {
+    slideShow.goForward();
+    visibleFrameCheck(i, i);
+  }
+
+  slideShow.goBack();
+  visibleFrameCheck(totalFrames, totalFrames - 1);
+
+  slideShow.goForward();
+  visibleFrameCheck(totalFrames, totalFrames);
+
+  /* last one is still visible since repeat is off */
+  slideShow.goForward();
+  visibleFrameCheck(totalFrames, totalFrames);
+
+  slideShow.repeat();
+  slideShow.goForward();
+  visibleFrameCheck(totalFrames, 1);
+
+  slideShow.goBack();
+  visibleFrameCheck(totalFrames, totalFrames);
+
+  slideShow.stopRepeating();
+  slideShow.goForward();
+  visibleFrameCheck(totalFrames, totalFrames);
+
 });
